be06990d0c8e6992351c370e86ffca0254c260ac
Brandon Kroes <b.kroes@protonmail.com>
2019-05-18 12:08:05 +0200

    initialised the test class for the in memory cell

---
+++ test/Cell/cell_inmemory_test.py
@@ -0,0 +1,17 @@
+import unittest
+from module.Structure import CellInMemory
+
+
+class TestCellInMemory(unittest.TestCase):
+    """
+    Basic test class for the in memory cell. Most cell classes will cohere to this basic implementation.
+    Contains:
+
+        - Accessibility access
+        - Constructor validation
+        - Getting the x_y list
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
