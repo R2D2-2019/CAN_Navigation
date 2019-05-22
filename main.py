153a968d86feaac04024a019ce9fdc70c1640ec8
nicovanbentum <niico0708@gmail.com>
2019-05-22 16:22:11 +0200

    Sending path steps on the bus now works, also added keyboard input to test this in realtime

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
+## @package main
+
 from time import sleep
 from sys import platform
 import signal
-
+import threading
+import queue
 from client.comm import Comm
-from module.module import Module
+from module.module import NavigationModule
 
-should_stop = False
+SHOULD_STOP = False
 
+# This function reads keyboard input from a queue.
+# https://stackoverflow.com/questions/5404068/how-to-read-keyboard-input/53344690#53344690
+def read_kbd_input(input_queue):
+    while True:
+        input_str = input()
+        input_queue.put(input_str)
+
+# This function acts as a symbolic main since Python doesn't actually utilize it
+#  in the same way C does.
 def main():
+    input_queue = queue.Queue()
+    input_thread = threading.Thread(
+        target=read_kbd_input, args=(input_queue,), daemon=True)
+    input_thread.start()
+
     print("Starting application...\n")
-    nav = Module(Comm())
+    nav = NavigationModule(Comm())
     print("Module created...")
 
     while not should_stop:
+        if (input_queue.qsize() > 0):
+            input_str = input_queue.get()
+            print("input was: {} ".format(input_str))
+            if input_str == "send":
+                nav.send_test_frames()
+
         nav.process()
         sleep(0.05)
 
     nav.stop()
 
+# This function stops processing CAN bus data
+# @param signal not sure why this is here
+# @param frame also not sure why this is here, might be added to the Python build later
+# to exit when it receives a certain frame.
 def stop(signal, frame):
     global should_stop
     should_stop = True
@@ -31,4 +58,3 @@ if platform != "win32":
 
 if __name__ == "__main__":
     main()
-
--- module/module.py
+++ module/module.py
@@ -1,27 +1,28 @@
+## @package mod
+
 from client.comm import BaseComm
 from common.frame_enum import FrameType
+from common.frames import FramePathStep
+
 from .Algorithms import AStar
 from .Structure import Grid
-
-# TODO: make a pull request for this frame type
-
-class move_instruction_c:
-    def __init__(self, path_id=0, id=0, x=0, y=0):
-        self.path_id = path_id
-        self.instruction_id = id
-        self.x = x
-        self.y = y
+from .Structure import grid_factory
 
 #TODO: documentation
 # TODO: Unit tests
 
-class Module:
-    def __init__(self, comm: BaseComm):
-        self.path_id = 0
-        self.comm = comm
-        # we'll need to figure out what frames to listen for
-        # self.comm.listen_for([FrameType.ACTIVITY_LED_STATE])
 
+## this class is based on the module template and implements the 3 template functions
+#  and additional functions that are needed.
+class NavigationModule:
+    ## The constructor
+    # @param comm variable that serves as an interface to the CAN bus
+    def __init__(self, comm: BaseComm):
+        self.comm = comm
+        self.comm.listen_for([FrameType.PATH_STEP])
+        self.path_id = 0
+
+    ## This function is what gets and puts data from/on the CAN bus
     def process(self):
         while self.comm.has_data():
             frame = self.comm.get_data()
@@ -30,23 +31,39 @@ class Module:
                 continue
 
             values = frame.get_data()
+            print("received: x is {} y is {} step_id is {} path id is {}".format(
+                values[0], values[1], values[2], values[3]))
 
-            if values[0]:
-                print("The LED is ON")
-            else:
-                print("The LED is OFF")
-
+    ## This function is called when we stop the application
     def stop(self):
         self.comm.stop()
 
-    def get_instruction_path(self, path, path_id):
-        instruction_path = list()
-        self.path_id += 1
-        instruction_id = 0
+    ## This function converts a path (list of coordinates) to a list of "frames".
+    # @return returns a list of frames that acts as a path.
+    def generate_frame_path(self, path):
+        frame_path = list()
+        step_id = 0
 
         for coord in path:
-            instruction = move_instruction_c(
-                path_id, instruction_id, coord[0], coord[1])
-            instruction_id += 1
-            instruction_path.append(instruction)
-        return instruction_path
+            frame = FramePathStep()
+            frame.set_data(coord[0], coord[1], step_id, self.path_id)
+            step_id += 1
+            frame_path.append(frame)
+
+        self.path_id += 1
+        return frame_path
+
+    ## This function sends a bunch of path step frames over the CAN bus
+    # mainly used to test for now, this will cleaned up once we have all the endpoints connected
+    # TODO: clean up
+    def send_test_frames(self):
+        g = grid_factory(10, 10)
+        start = g[(0, 0)]
+        end = g[(5, 2)]
+        a_star = AStar(g, start, end)
+        print(a_star.solve())
+        frame_path = self.generate_frame_path(a_star.solve())
+        for frame in frame_path:
+            self.comm.send(frame)
+            data = frame.get_data()
+            print("sending: x is {} y is {} step_id is {} path_id is {}".format(data[0], data[1], data[2], data[3]))
