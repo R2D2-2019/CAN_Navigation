24a8a28b97b7355588482880391d207e423af7b7
Nico van Bentum <31741718+nicovanbentum@users.noreply.github.com>
2019-05-24 02:22:43 +0200

    integrating all the new frames partly done

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-## @package main
-
 from time import sleep
 from sys import platform
 import signal
@@ -10,16 +8,25 @@ from module.mod import NavigationModule
 
 SHOULD_STOP = False
 
-# This function reads keyboard input from a queue.
-# https://stackoverflow.com/questions/5404068/how-to-read-keyboard-input/53344690#53344690
+
 def read_kbd_input(input_queue):
+    """
+    This function reads keyboard input from a queue.
+    https://stackoverflow.com/questions/5404068/how-to-read-keyboard-input/53344690#53344690
+
+    :param input_queue: python queue that holds the characters put in
+    """
     while True:
         input_str = input()
         input_queue.put(input_str)
 
-# This function acts as a symbolic main since Python doesn't actually utilize it
-#  in the same way C does.
+
 def main():
+    """
+    This function acts as a symbolic main since Python doesn't actually utilize it
+    in the same way C does. 
+    """
+    global SHOULD_STOP
     input_queue = queue.Queue()
     input_thread = threading.Thread(
         target=read_kbd_input, args=(input_queue,), daemon=True)
@@ -30,24 +37,27 @@ def main():
     print("Module created...")
 
     while not SHOULD_STOP:
+        # did this so i can exit cleanly,
+        # for some reason i can't ctrl c out of this python application.
         if (input_queue.qsize() > 0):
             input_str = input_queue.get()
-            print("input was: {} ".format(input_str))
-            if input_str == "send":
-                nav.send_test_frames()
+            if input_str == "exit":
+                SHOULD_STOP = True
 
         nav.process()
+        if nav.requested:
+            nav.process_request()
         sleep(0.05)
 
     nav.stop()
 
-# This function stops processing CAN bus data
-# @param signal not sure why this is here
-# @param frame also not sure why this is here, might be added to the Python build later
-# to exit when it receives a certain frame.
+
 def stop(signal, frame):
-    global should_stop
-    should_stop = True
+    """ This function stops processing CAN bus data
+    :param signal: not used
+    :param frame: not used """
+    global SHOULD_STOP
+    SHOULD_STOP = True
 
 
 signal.signal(signal.SIGINT, stop)
