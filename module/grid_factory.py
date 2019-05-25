8b49b742d26bcc3d45838fef119c40006f7778d8
Brandon Kroes <b.kroes@protonmail.com>
2019-05-25 17:32:55 +0200

    In hindsight it isn't a nice way to check if we have enough memory by filling our memory and seeing if an error occurs. so we're not estemating based on 'available' memory

--- module/grid_factory.py
+++ module/grid_factory.py
@@ -1,10 +1,26 @@
-from module.Structure import GridInFile, GridInMemory
+from module.Structure import GridInMemory
+from module.Structure import GridInFile
+from module.Structure import GridInNumpy
+
+
+def calculate_potential_size(columns, rows):
+    # TODO: Instantiate cells for a better idea of size
+    """ ROUGH ESTIMATION, UNRELIABLE IN NATURE"""
+
+    overhead = 100000  # meant as a SOFT indication on what we need for the actual algorithm operation
+
+    var_size = 64  # max amount of bits that are expected to be used to store
+    # 5 is the amount of variables stored (x,y,f,g,h)
+    return overhead + ((5 * (columns * rows)) * var_size)
 
 
 def grid_factory(columns=0, rows=0):
-    try:
-        g = GridInMemory(columns, rows)
-    except MemoryError:
-        g = GridInFile(columns, rows)
+    # Trying if we can actually do it memory wise:
+    import os
+    import psutil
 
-    return g
+    process = psutil.Process(os.getpid())
+    available_bits = process.memory_info().rss * 8  # the amount of "free" memory we have for our process
+    if available_bits < calculate_potential_size(rows, columns):
+        return GridInFile(columns, rows)
+    return GridInMemory(columns, rows)
