0dc58196e80f5a1cbefbb1136607586baba6ad6a
Brandon Kroes <b.kroes@protonmail.com>
2019-05-26 20:00:10 +0200

    Debugging issue so the previous is set to an object, not the cooridnates

--- module/Cell.py
+++ module/Cell.py
@@ -73,7 +73,7 @@ class CellInMemory(Cell):
 
         """
         if self.get_x_y() != item.get_x_y():
-            self.previous = item.get_x_y()
+            self.previous = item
 
     def __getattr__(self, key):
         return object.__getattribute__(self, key)
