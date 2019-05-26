18e144706f5731daf6142ddba0a49d7d5d0b9b39
Nico van Bentum <31741718+nicovanbentum@users.noreply.github.com>
2019-05-27 00:28:30 +0200

    matplotlib static and animated done

---
+++ Visualizer/matplot.py
@@ -0,0 +1,71 @@
+from pipper import install_packages
+install_packages(['matplotlib'])
+
+from modules.CAN_Navigation.module.Structure import grid_factory
+from modules.CAN_Navigation.module.Algorithms import AStar
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+
+
+
+def input_to_list_of_ints(string: str, delimiter: chr):
+    """
+    Converts input to a list of strings
+    :param string: String of characters to print to the user
+    :param delimiter: Character that determines when to slice
+    """
+    input_string = input(string)
+    return list(map(int, input_string.split(delimiter)))
+
+
+def update_graph(c, ax, x_points, y_points):
+    """ 
+    This updates the graph every frame
+    :param c: matplotlib uses this as frame counter
+    :param ax: axis we draw to
+    :param x_points: x values of every vector2 in a path
+    :param y_points: y values of every vector2 in a path
+    """
+    x, y = list(), list()
+    for i in range(c):
+        x.append(x_points[i])
+        y.append(y_points[i])
+    ax.plot(x, y, 'r--')
+
+
+def main():
+    """
+    Symbolic main function that does all the magic
+    """
+    grid_size = input_to_list_of_ints("grid size: ", ',')
+    start = input_to_list_of_ints("start vector: ", ',')
+    end = input_to_list_of_ints("end vector: ", ',')
+    animated = input("animated? y/n : ")
+    animated = True if animated == 'y' else False
+
+    figure = plt.figure()
+    axes = figure.add_subplot()
+    plt.axis([0, grid_size[0], 0, grid_size[1]])
+
+    grid = grid_factory(grid_size[0], grid_size[1])
+    start_point = grid[(start[0], start[1])]
+    end_point = grid[(end[0], end[1])]
+    algo = AStar(grid, start_point, end_point)
+    path = algo.solve()
+
+    x_points, y_points = list(), list()
+    for vector2 in path:
+        x_points.append(vector2[0])
+        y_points.append(vector2[1])
+
+    if animated:
+        ani = animation.FuncAnimation(figure, update_graph, interval=50, fargs=[
+                                      axes, x_points, y_points], frames=len(x_points), repeat=False)
+    else:
+        axes.plot(x_points, y_points, 'r--')
+
+    axes.grid()
+    plt.show()
+
+
+main()
