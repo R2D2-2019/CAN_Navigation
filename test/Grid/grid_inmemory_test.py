e3be29259943596c27cdceedb4623185edeff83e
Brandon Kroes <b.kroes@protonmail.com>
2019-05-27 14:19:01 +0200

    applying the new accessible call to inmemory test cases

--- test/Grid/grid_inmemory_test.py
+++ test/Grid/grid_inmemory_test.py
@@ -33,7 +33,7 @@ class TestGridInMemory(unittest.TestCase):
 
         for x in range(rows - 1):
             for y in range(cols - 1):
-                g[(x, y)].accessible = z[x][y]
+                g[(x, y)].set_accessible(z[x][y])
 
         found = False
         for x in range(rows - 1):
@@ -64,7 +64,7 @@ class TestGridInMemory(unittest.TestCase):
 
         for x in range(rows - 1):
             for y in range(cols - 1):
-                g[(x, y)].accessible = z[x][y]
+                g[(x, y)].set_accessible(z[x][y])
 
         self.assertEqual(g[(int(rows / 2), int(cols / 2))
                            ].accessible, z[int(rows / 2)][int(cols / 2)])
