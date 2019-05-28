357ee19c258adc78c884e46d145b6413953760ba
Nico van Bentum <31741718+nicovanbentum@users.noreply.github.com>
2019-05-28 13:20:09 +0200

    optimized for grouped pygame sprites and removed base class

--- Visualizer/rectangle.py
+++ Visualizer/rectangle.py
@@ -1,47 +1,35 @@
 from pipper import install_packages
-install_packages(['pygame', 'euclid3'])
+install_packages(['pygame'])
 
 import pygame as pg
-import euclid3 as vmath
 
-class Drawable:
-    """
-    Abstract class only for overriding by a child class.
-    """
-    def __init__(self):
-        pass
-    
-    def draw(self, window):
-        pass
-
-class Rectangle(Drawable):
+class Rectangle(pg.sprite.Sprite):
     """
     Square shape used in our AStar visualization. Provides an easy way to draw a rectangle shape using pygame.
     Uses the euclid3 module for vector2.
     """
-    def __init__(self, pos: vmath.Vector2, size: vmath.Vector2):
+    def __init__(self, pos: list, size: list):
         """
         Default and only constructor.
         :param pos: euclid3 vector2 for window position.
         :param size: euclid3 vector2 for size in pixels.
         """
-        Drawable.__init__(self)
-        self.pos = pos
-        self.size = size
-        self.body = pg.Rect(self.pos.x, self.pos.y, self.size.x, self.size.y)
+        super().__init__()
+        self.image = pg.Surface([size[0], size[1]])
         self.color = (255, 255, 255)
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
         self.obstacle = False
         self.start = False
         self.end = False
 
-    def draw(self, window):
-        """ override from base class. """
-        pg.draw.rect(window, self.color, (self.pos.x, self.pos.y, self.size.x, self.size.y))
-
     def set_color(self, color: list):
         """ sets the rectangle color. """
         self.color = color
+        self.image.fill(self.color)
 
     def mark_obstacle(self):
         """ Marks the rectangle as obstacle and changes the color.  """
@@ -52,17 +40,17 @@ class Rectangle(Drawable):
         """ Marks the rectangle as the start cell and changes the color. """
         self.obstacle = False
         self.start = True
-        self.color = (0, 255, 0)
+        self.set_color((0, 255, 0))
 
     def mark_end(self):
         """ Marks the rectangle as the end cell and changes the color. """
         self.obstacle = False
         self.end = True
-        self.color = (255, 0, 0)
+        self.set_color((255, 0, 0))
 
     def reset(self):
         """ Resets the  variables so it can be used again for pathfinding. """
         self.obstacle = False
-        self.color = (255, 255, 255)
+        self.set_color((255, 255, 255))
         self.start = False
         self.end = False
